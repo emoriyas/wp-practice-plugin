300d56abf9dd94684151333a24c6b2c8204c69e9
airwick <ehddnr85963@gmail.com>
2020-11-28 16:43:48 -1000

    Add tag checks in post level.

--- wp-practice-plugin.php
+++ wp-practice-plugin.php
@@ -6,6 +6,9 @@
      * Author: Author
      */
 
+    $accepted_tags = array("tag1", "tag3", "tag4");
+    $html = "<p>Hello World!</p>";
+
     __initialize();
 
     function __initialize() {
@@ -19,7 +22,16 @@
      * @param $post Post object.
      */
     function wppp_add_html_control ($post) {
-        add_action( 'the_content', 'wppp_add_html_content');
+        global $accepted_tags;
+        $tags = get_the_tags($post->ID);
+
+        foreach ($tags as $tag) {
+            foreach ($accepted_tags as $acceptTag) {
+                if (strcmp($tag->name, $acceptTag->name) == 0) {
+                    add_action( 'the_content', 'wppp_add_html_content');
+                }
+            }
+        }
     }
 
     /**
